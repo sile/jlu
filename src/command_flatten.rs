78424676095c08aad54698a7546c9cd4fbb0cdb6
Takeru Ohta <phjgt308@gmail.com>
2024-07-03 12:07:00 +0900

    Implement flatten command

--- src/command_flatten.rs
+++ src/command_flatten.rs
@@ -1,6 +1,6 @@
 use crate::jsonl;
 use orfail::OrFail;
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 #[derive(Debug, clap::Args)]
 pub struct FlattenCommand {}
@@ -9,13 +9,44 @@ impl FlattenCommand {
     pub fn run(&self) -> orfail::Result<()> {
         for result in jsonl::read_from_stdin() {
             let value = result?;
-            let flattened_values = flatten(value);
-            jsonl::write_to_stdout(flattened_values).or_fail()?;
+            let mut output = Map::new();
+            self.flatten(value, "", &mut output);
+            jsonl::write_to_stdout(std::iter::once(Value::Object(output))).or_fail()?;
         }
         Ok(())
     }
-}
 
-fn flatten(value: Value) -> impl Iterator<Item = Value> {
-    vec![value].into_iter()
+    fn flatten(&self, value: Value, prefix: &str, output: &mut Map<String, Value>) {
+        match value {
+            Value::Null => {
+                output.insert(prefix.to_owned(), Value::Null);
+            }
+            Value::Bool(v) => {
+                output.insert(prefix.to_owned(), Value::Bool(v));
+            }
+            Value::Number(v) => {
+                output.insert(prefix.to_owned(), Value::Number(v));
+            }
+            Value::String(v) => {
+                output.insert(prefix.to_owned(), Value::String(v));
+            }
+            Value::Array(array) => {
+                let width = array.len().saturating_sub(1).to_string().len();
+                for (i, v) in array.into_iter().enumerate() {
+                    let key = format!("{prefix}[{i:0width$}]");
+                    self.flatten(v, &key, output);
+                }
+            }
+            Value::Object(object) => {
+                for (k, v) in object {
+                    let key = if prefix.is_empty() {
+                        k.clone()
+                    } else {
+                        format!("{prefix}.{k}")
+                    };
+                    self.flatten(v, &key, output);
+                }
+            }
+        }
+    }
 }
