2adc02bafa19970a5b12a14e4496768438b47f20
Takeru Ohta <phjgt308@gmail.com>
2024-07-03 21:03:05 +0900

    Implement count command

--- src/command_count.rs
+++ src/command_count.rs
@@ -1,57 +1,74 @@
 use crate::jsonl;
+use orfail::OrFail;
+use serde_json::Value;
+use std::collections::BTreeMap;
+use std::io::Write;
 
 #[derive(Debug, clap::Args)]
 pub struct CountCommand {
-    pub keys: Vec<String>,
+    pub names: Vec<String>,
 }
 
 impl CountCommand {
     pub fn run(&self) -> orfail::Result<()> {
+        let mut counter = Counter::new();
         for result in jsonl::from_stdin() {
-            let _value = result?;
+            let value = result?;
+            counter.increment(&mut self.names.iter(), &value);
         }
+
+        let stdout = std::io::stdout();
+        let mut stdout = stdout.lock();
+        serde_json::to_writer_pretty(&mut stdout, &counter).or_fail()?;
+        writeln!(&mut stdout).or_fail()?;
+
         Ok(())
     }
 }
 
-// #[derive(Debug, serde::Serialize)]
-// #[serde(untagged)]
-// enum Counter {
-//     Value(usize),
-//     Children(BTreeMap<String, Self>),
-// }
+#[derive(Debug, serde::Serialize)]
+#[serde(untagged)]
+enum Counter {
+    Value(usize),
+    Children(BTreeMap<String, Self>),
+}
 
-// impl Counter {
-//     fn new() -> Self {
-//         Self::Value(0)
-//     }
+impl Counter {
+    fn new() -> Self {
+        Self::Value(0)
+    }
 
-//     fn increment<'a>(
-//         &mut self,
-//         fields: &'a mut impl Iterator<Item = &'a String>,
-//         message: &Message,
-//     ) {
-//         let Some(field) = fields.next() else {
-//             let Self::Value(count) = self else {
-//                 unreachable!();
-//             };
-//             *count += 1;
-//             return;
-//         };
+    fn increment<'a>(&mut self, names: &'a mut impl Iterator<Item = &'a String>, value: &Value) {
+        let Some(name) = names.next() else {
+            let Self::Value(count) = self else {
+                unreachable!();
+            };
+            *count += 1;
+            return;
+        };
 
-//         let key = message
-//             .get_value_string(field)
-//             .unwrap_or_else(|| "_OTHER_".to_string());
+        let key = value
+            .as_object()
+            .and_then(|object| object.get(name))
+            .and_then(|key| match key {
+                Value::Null => Some("null".to_string()),
+                Value::Bool(v) => Some(v.to_string()),
+                Value::Number(v) => Some(v.to_string()),
+                Value::String(v) => Some(v.clone()),
+                Value::Array(_) => None,
+                Value::Object(_) => None,
+            })
+            .unwrap_or_else(|| "_OTHER_".to_string());
 
-//         if let Self::Value(_) = self {
-//             *self = Self::Children(BTreeMap::new());
-//         }
-//         let Self::Children(children) = self else {
-//             unreachable!();
-//         };
-//         children
-//             .entry(key.to_string())
-//             .or_insert_with(Self::new)
-//             .increment(fields, message);
-//     }
-// }
+        if let Self::Value(_) = self {
+            *self = Self::Children(BTreeMap::new());
+        }
+        let Self::Children(children) = self else {
+            unreachable!();
+        };
+        children
+            .entry(key)
+            .or_insert_with(Self::new)
+            .increment(names, value);
+    }
+}
