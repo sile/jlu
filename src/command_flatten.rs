3e21effb68c48672cbb76787ff8807faad18b06e
Takeru Ohta <phjgt308@gmail.com>
2024-07-03 09:18:00 +0900

    [WIP] Add flatten()

--- src/command_flatten.rs
+++ src/command_flatten.rs
@@ -1,4 +1,6 @@
 use crate::jsonl;
+use orfail::OrFail;
+use serde_json::Value;
 
 #[derive(Debug, clap::Args)]
 pub struct FlattenCommand {}
@@ -6,8 +8,14 @@ pub struct FlattenCommand {}
 impl FlattenCommand {
     pub fn run(&self) -> orfail::Result<()> {
         for result in jsonl::read_from_stdin() {
-            let _value = result?;
+            let value = result?;
+            let flattened_values = flatten(value);
+            jsonl::write_to_stdout(flattened_values).or_fail()?;
         }
         Ok(())
     }
+}
+
+fn flatten(value: Value) -> impl Iterator<Item = Value> {
+    vec![value].into_iter()
 }
