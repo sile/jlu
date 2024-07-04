60cd21e5dc5fdc8547ce52e3d9849a470b0258fe
Takeru Ohta <phjgt308@gmail.com>
2024-07-04 10:09:09 +0900

    Implement table command

--- src/command_table.rs
+++ src/command_table.rs
@@ -1,63 +1,65 @@
-use crate::jsonl;
+use crate::jsonl::{self, Object};
 use orfail::OrFail;
+use serde_json::Value;
 use std::collections::BTreeMap;
 
 #[derive(Debug, clap::Args)]
 pub struct TableCommand {
     #[clap(long, short, default_value_t = 50)]
-    pub max_column_width: usize,
+    pub max_column_chars: usize,
     pub column_keys: Vec<String>,
 }
 
 impl TableCommand {
     pub fn run(&self) -> orfail::Result<()> {
-        // let mut columns = self
-        //     .column_keys
-        //     .iter()
-        //     .map(|key| Column::new(key))
-        //     .collect::<Vec<_>>();
-        // let mut rows = Vec::new();
-        // for message in jsonl::input_items::<JsonMap>() {
-        //     let message = message.or_fail()?;
-        //     let mut row = BTreeMap::new();
-        //     for column in &mut columns {
-        //         let mut value =
-        //             json_value_to_string(&message.get(&column.key).cloned().unwrap_or_default());
-        //         if value.len() > self.max_column_width {
-        //             value.truncate(self.max_column_width);
-        //             value.push_str("...");
-        //         }
+        let mut columns = self
+            .column_keys
+            .iter()
+            .map(|key| Column::new(key))
+            .collect::<Vec<_>>();
+        let mut rows = Vec::new();
+        for result in jsonl::from_stdin::<Object>() {
+            let object = result.or_fail()?;
+            let mut row = BTreeMap::new();
+            for column in &mut columns {
+                let mut value = json_value_to_string(object.get(&column.key));
+                if value.chars().count() > self.max_column_chars {
+                    let (n, _) = value.char_indices().nth(self.max_column_chars).or_fail()?;
+                    value.truncate(n);
+                    value.push_str("...");
+                }
 
-        //         column.update_width(&value);
-        //         row.insert(column.key.clone(), value);
-        //     }
-        //     rows.push(row);
-        // }
+                column.update_width(&value);
+                row.insert(column.key.clone(), value);
+            }
+            rows.push(row);
+        }
 
-        // rows.sort_by(|x, y| {
-        //     let xs = columns.iter().map(|c| x.get(&c.key));
-        //     let ys = columns.iter().map(|c| y.get(&c.key));
-        //     xs.cmp(ys)
-        // });
+        rows.sort_by(|x, y| {
+            let xs = columns.iter().map(|c| x.get(&c.key));
+            let ys = columns.iter().map(|c| y.get(&c.key));
+            xs.cmp(ys)
+        });
 
-        // for col in &columns {
-        //     print!("| {:<width$} ", col.key, width = col.width);
-        // }
-        // println!("|");
+        for col in &columns {
+            print!("| {:<width$} ", col.key, width = col.width);
+        }
+        println!("|");
 
-        // for col in &columns {
-        //     print!("|-{:-<width$}-", "-", width = col.width);
-        // }
-        // println!("|");
+        for col in &columns {
+            print!("|-{:-<width$}-", "-", width = col.width);
+        }
+        println!("|");
 
-        // let null = "".to_string();
-        // for row in rows {
-        //     for col in &columns {
-        //         let value = row.get(&col.key).unwrap_or(&null);
-        //         print!("| {:<width$} ", value, width = col.width);
-        //     }
-        //     println!("|");
-        // }
+        let null = "".to_string();
+        for row in rows {
+            for col in &columns {
+                let value = row.get(&col.key).unwrap_or(&null);
+                print!("| {:<width$} ", value, width = col.width);
+            }
+            println!("|");
+        }
+
         Ok(())
     }
 }
@@ -81,16 +83,16 @@ impl Column {
     }
 }
 
-fn json_value_to_string(v: &serde_json::Value) -> String {
+fn json_value_to_string(v: Option<&Value>) -> String {
+    let Some(v) = v else {
+        return "".to_string();
+    };
     match v {
-        serde_json::Value::Null => "".to_string(),
-        serde_json::Value::Bool(v) => v.to_string(),
-        serde_json::Value::Number(v) => v.to_string(),
-        serde_json::Value::String(v) => v.replace('|', "\\|"),
-        serde_json::Value::Array(v) => {
-            let v = v.iter().map(json_value_to_string).collect::<Vec<_>>();
-            v.join("_")
-        }
-        serde_json::Value::Object(_) => "<object>".to_string(),
+        Value::Null => "null".to_string(),
+        Value::Bool(v) => v.to_string(),
+        Value::Number(v) => v.to_string(),
+        Value::String(v) => v.replace('|', "\\|"),
+        Value::Array(_) => "_ARRAY_".to_string(),
+        Value::Object(_) => "_OBJECT_".to_string(),
     }
 }
