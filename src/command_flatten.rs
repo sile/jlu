8a2f4602fbe77fa03ed5217e841e4dc8d94e7b08
Takeru Ohta <phjgt308@gmail.com>
2024-07-04 16:07:36 +0900

    Fix clippy warning

--- src/command_flatten.rs
+++ src/command_flatten.rs
@@ -10,43 +10,43 @@ impl FlattenCommand {
         let output_values = jsonl::from_stdin().map(|input_value| {
             let input_value = input_value.or_fail()?;
             let mut flattened = Map::new();
-            self.flatten(input_value, "", &mut flattened);
+            flatten(input_value, "", &mut flattened);
             Ok(Value::Object(flattened))
         });
         jsonl::to_stdout(output_values).or_fail()?;
         Ok(())
     }
+}
 
-    fn flatten(&self, value: Value, prefix: &str, output: &mut Map<String, Value>) {
-        match value {
-            Value::Null => {
-                output.insert(prefix.to_owned(), Value::Null);
+fn flatten(value: Value, prefix: &str, output: &mut Map<String, Value>) {
+    match value {
+        Value::Null => {
+            output.insert(prefix.to_owned(), Value::Null);
+        }
+        Value::Bool(v) => {
+            output.insert(prefix.to_owned(), Value::Bool(v));
+        }
+        Value::Number(v) => {
+            output.insert(prefix.to_owned(), Value::Number(v));
+        }
+        Value::String(v) => {
+            output.insert(prefix.to_owned(), Value::String(v));
+        }
+        Value::Array(array) => {
+            let width = array.len().saturating_sub(1).to_string().len();
+            for (i, v) in array.into_iter().enumerate() {
+                let key = format!("{prefix}[{i:0width$}]");
+                flatten(v, &key, output);
             }
-            Value::Bool(v) => {
-                output.insert(prefix.to_owned(), Value::Bool(v));
-            }
-            Value::Number(v) => {
-                output.insert(prefix.to_owned(), Value::Number(v));
-            }
-            Value::String(v) => {
-                output.insert(prefix.to_owned(), Value::String(v));
-            }
-            Value::Array(array) => {
-                let width = array.len().saturating_sub(1).to_string().len();
-                for (i, v) in array.into_iter().enumerate() {
-                    let key = format!("{prefix}[{i:0width$}]");
-                    self.flatten(v, &key, output);
-                }
-            }
-            Value::Object(object) => {
-                for (k, v) in object {
-                    let key = if prefix.is_empty() {
-                        k.clone()
-                    } else {
-                        format!("{prefix}.{k}")
-                    };
-                    self.flatten(v, &key, output);
-                }
+        }
+        Value::Object(object) => {
+            for (k, v) in object {
+                let key = if prefix.is_empty() {
+                    k.clone()
+                } else {
+                    format!("{prefix}.{k}")
+                };
+                flatten(v, &key, output);
             }
         }
     }
